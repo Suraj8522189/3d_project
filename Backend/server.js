811954b056d1f5d3ba38bf971b087ed01ea62fd6
Suraj8522189 <sr0663784@gmail.com>
2026-02-23 23:15:39 +0530

    add

--- Backend/server.js
+++ Backend/server.js
@@ -8,19 +8,27 @@ import settingsRoutes from "./src/routes/settingsRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+// CORS (Frontend allow)
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB error:", err));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.error("MongoDB error ❌", err.message));
+
 
 app.use("/api/upload", uploadRoutes);
 app.use("/api/settings", settingsRoutes);
 
-app.get("/", (req, res) => res.send("API running"));
-
+app.get("/", (req, res) => res.send("API running ✅"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
