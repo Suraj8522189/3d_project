bbe4f687aace4f11ed16a78e38f2a084d13e1b41
Suraj8522189 <sr0663784@gmail.com>
2026-02-23 23:28:16 +0530

    addon

--- Backend/server.js
+++ Backend/server.js
@@ -8,22 +8,22 @@ import settingsRoutes from "./src/routes/settingsRoutes.js";
 dotenv.config();
 const app = express();
 
-// CORS (Frontend allow)
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://3d-project-pi.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  credentials: true
 }));
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected ✅"))
   .catch((err) => console.error("MongoDB error ❌", err.message));
-
 
 app.use("/api/upload", uploadRoutes);
 app.use("/api/settings", settingsRoutes);
@@ -31,4 +31,4 @@ app.use("/api/settings", settingsRoutes);
 app.get("/", (req, res) => res.send("API running ✅"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
